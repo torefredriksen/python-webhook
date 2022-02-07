358771ae6c4b19a149a26a573565753d1f65e565
Tore Fredriksen <torefr@gmail.com>
2022-02-07 12:08:07 +0100

    initial commit

---
+++ README.server.py
@@ -0,0 +1,9 @@
+https://devopslifecycle.com/lessons/3/receiving-webhooks-with-python
+
+# install flask library
+pip install flask
+
+# run the webserver that will receive the webhook (post request)
+python server.py
+
+
